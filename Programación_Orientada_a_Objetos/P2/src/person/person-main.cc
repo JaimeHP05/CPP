f603acdf29a02825fc1a362253b326c46a1aba7d
Jaime Hernández Pérez <i32hepej@uco.es>
2025-05-06 12:28:50 +0200

    Update person-main.cc

--- Programación_Orientada_a_Objetos/P2/src/person/person-main.cc
+++ Programación_Orientada_a_Objetos/P2/src/person/person-main.cc
@@ -1,1 +1,37 @@
+#include "person.h"
+#include <iostream>
 
+namespace ns1{
+	int a;
+	int b;
+}
+namespace ns2{
+	float a;
+	float c;
+}
+
+int main() {
+	int a=55;
+	ns1::a=0;
+	ns2::a=2.3;
+	std::cout<< "ns1::a= " << ns1::a << "\n";
+	std::cout<< "ns2::a= " << ns2::a << "\n";
+	std::cout<< "a= " << a << "\n";
+	Person p1("Juan", 32, 4.568), p2("Ana", 41, 7.371);
+	std::cout << p1.GetName() << "\n";
+	std::cout << p1.GetAge() << std::endl;
+	std::cout << p1.GetRank() << std::endl;
+	std::cout << p2.GetName() << "\n";
+	std::cout << p2.GetAge() << std::endl;
+	std::cout << p2.GetRank() << std::endl;
+	p1.AddPreferences("Hola", "Adios", "Ok");
+	p2.AddPreferences("Perfecto", "Gracias", "Buenas");
+	p1.ShowPreferences();
+	p2.ShowPreferences();
+	p1.AddPreference("Pepito");
+	p2.AddPreference("Juanito");
+	p1.ChangePreference(1, "Nuevo");
+	p2.ChangePreference(1, "New");
+	p1.ShowPreferences();
+	p2.ShowPreferences();
+}
