2bca8c63f5e938403a1bc4a3e70077e7b9e1502f
Jaime Hernández Pérez <i32hepej@uco.es>
2025-05-06 12:28:10 +0200

    Update person.cc

--- Programación_Orientada_a_Objetos/P2/src/person/person.cc
+++ Programación_Orientada_a_Objetos/P2/src/person/person.cc
@@ -1,1 +1,22 @@
+#include "person.h"
+#include <iostream>
+#include <string>
+#include <vector>
 
+void Person::AddPreferences(std::string s1, std::string s2, std::string s3){
+	preferences_.insert(preferences_.begin(), s1);
+	preferences_.insert(preferences_.end(), s3);
+	preferences_.insert(preferences_.begin()+(preferences_.size()/2), s2);
+}
+
+void Person::ShowPreferences(){
+	std::cout << "\nLas preferencias son: " << std::endl;
+	for (auto s: preferences_){
+		std::cout << s << std::endl;
+	}
+}
+
+void Person::ChangePreference(int posicion, std::string nuevo){ 
+	try {preferences_.at(posicion) = nuevo;}
+	catch (std::out_of_range const & exc) {std::cout << exc.what() << "\n";}
+}
