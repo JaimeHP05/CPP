3a4c952cde9cb8b2cfd0b24a3e3325904eb6bc67
Jaime Hernández Pérez <i32hepej@uco.es>
2025-05-14 13:43:31 +0200

    Update computer-main.cc

--- Programación_Orientada_a_Objetos/P4/marketplace/src/computer/computer-main.cc
+++ Programación_Orientada_a_Objetos/P4/marketplace/src/computer/computer-main.cc
@@ -1,1 +1,6 @@
-
+#include "computer.h"
+#include <iostream>
+int main() {
+	Computer p1("111Q", ComputerType::Gaming, "PC Ultra", 33, "Windows", "MediaMarkt");
+	std::cout << p1.GetDataStr() << std::endl;
+}
