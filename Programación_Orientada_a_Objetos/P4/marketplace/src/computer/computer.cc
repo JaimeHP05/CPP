5c1a9bf5ff5bc65c58e47182d919722d87309ef1
Jaime Hernández Pérez <i32hepej@uco.es>
2025-05-14 13:43:45 +0200

    Update computer.cc

--- Programación_Orientada_a_Objetos/P4/marketplace/src/computer/computer.cc
+++ Programación_Orientada_a_Objetos/P4/marketplace/src/computer/computer.cc
@@ -1,1 +1,10 @@
+#include "computer.h"
 
+Computer::Computer(std::string id, 
+ComputerType type,
+std::string name, 
+float price, 
+std::string maker, 
+std::string seller) : Product(id, name, price, maker, seller){
+	type_=type;
+}
