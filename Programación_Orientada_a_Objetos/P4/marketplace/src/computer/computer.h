9d885fd9830bf07687b02c0fff7b3431dd5ad4e6
Jaime Hernández Pérez <i32hepej@uco.es>
2025-05-14 13:43:57 +0200

    Update computer.h

--- Programación_Orientada_a_Objetos/P4/marketplace/src/computer/computer.h
+++ Programación_Orientada_a_Objetos/P4/marketplace/src/computer/computer.h
@@ -1,1 +1,22 @@
+#ifndef COMPUTER_H
+#define COMPUTER_H
+#include "product.h"
 
+enum class ComputerType {
+	Desktop,
+	Laptop,
+	Server,
+	Tablet,
+	Gaming
+};
+class Computer: public Product{
+	private:
+		ComputerType type_;
+	public:
+		Computer(std::string id_, ComputerType type_, std::string name_="empty", float price_=0.0, std::string maker_="empty", std::string seller_="empty");
+		ComputerType GetType(){return type_;}
+		void SetType(ComputerType nuevo_type) {
+			type_=nuevo_type;
+		}
+};
+#endif
