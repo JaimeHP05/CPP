cffbd99ce4de275cebb57ad7fffee5c6338a6d74
Jaime Hernández Pérez <i32hepej@uco.es>
2025-05-20 11:20:41 +0200

    Update tv.h

--- Programación_Orientada_a_Objetos/P4/marketplace/src/tv/tv.h
+++ Programación_Orientada_a_Objetos/P4/marketplace/src/tv/tv.h
@@ -1,1 +1,13 @@
+#ifndef TV_H
+#define TV_H
+#include "product.h"
 
+class Tv: public Product{
+	private:
+		float inch_;
+	public:
+		Tv(std::string id_, std::string name_="empty", float price_=0.0, std::string maker_="empty", std::string seller_="empty", float inch_=0.0);
+		float GetInch(){return inch_;}
+		bool SetInch(float nuevo_inch);
+};
+#endif
