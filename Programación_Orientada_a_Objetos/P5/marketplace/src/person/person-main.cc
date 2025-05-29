c41932028e2629f1cea1c2b68337a15ba85784f3
Jaime Hernández Pérez <i32hepej@uco.es>
2025-05-29 17:57:07 +0200

    Update person-main.cc

--- Programación_Orientada_a_Objetos/P5/marketplace/src/person/person-main.cc
+++ Programación_Orientada_a_Objetos/P5/marketplace/src/person/person-main.cc
@@ -1,1 +1,8 @@
+#include "person.h"
+#include <iostream>
 
+int main() {
+	Person p1("123A", "Jaime", "Córdoba", "Córdoba", "España", 19, 2.29, 2005), p2("456B", "Nicolás", "Pedregalejo", "Málaga", "España", 17, 8.31, 2003);
+	std::cout << p1.GetDataStr() << std::endl;
+	std::cout << p2.GetDataStr() << std::endl;
+}
