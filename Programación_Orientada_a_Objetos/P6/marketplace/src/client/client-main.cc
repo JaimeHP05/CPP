c2b412d0fb4ae29e590c6348b344c42d232f5bd7
Jaime Hernández Pérez <i32hepej@uco.es>
2025-06-09 11:53:00 +0200

    Update client-main.cc

--- Programación_Orientada_a_Objetos/P6/marketplace/src/client/client-main.cc
+++ Programación_Orientada_a_Objetos/P6/marketplace/src/client/client-main.cc
@@ -1,1 +1,9 @@
+#include "client.h"
+#include <iostream>
 
+int main() {
+	Client client("123", "John Doe", "New York", "NY", "USA", 30, 4.5, 2020, 1);
+	std::cout << "Client Name: " << client.GetName() << std::endl;
+	std::cout << "Client Premium Level: " << client.GetPremium() << std::endl;
+	return 0;
+}
