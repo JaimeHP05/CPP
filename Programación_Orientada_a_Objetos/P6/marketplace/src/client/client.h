6d68f88ff41b8b4179c7cd66545250182b823d00
Jaime Hernández Pérez <i32hepej@uco.es>
2025-06-09 11:53:16 +0200

    Update client.h

--- Programación_Orientada_a_Objetos/P6/marketplace/src/client/client.h
+++ Programación_Orientada_a_Objetos/P6/marketplace/src/client/client.h
@@ -1,1 +1,25 @@
+#ifndef CLIENT_H
+#define CLIENT_H
+#include "person.h"
+#include "basket.h"
 
+class Client: public Person, public Basket {
+	private:
+		int premium_;
+	public:
+		Client(std::string id,
+		std::string name="empty",
+		std::string town="empty",
+		std::string province="empty",
+		std::string country="empty",
+		int age = 0,
+		double rank = 0.00,
+		int entry_year = 0, 
+		int premium=0):
+		Person(id, name, town, province, country, age, rank, entry_year){
+			premium_=premium;
+		}
+		int GetPremium();
+		void SetPremium(int new_premium);
+};
+#endif
